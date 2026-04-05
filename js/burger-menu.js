11fa587b64ad9eedd808ae9b2be74db60844424d
4Davve <vadim.veng@gmail.com>
2026-04-05 23:36:53 +0200

    Update burger menu 05.04.2026

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,17 +1,20 @@
-const menu = document.querySelector(".slider");
+const menu = document.querySelector(".burger-menu");
 const openMenu = document.querySelector(".header__burger");
-const closeMenu = document.querySelector(".slider__btn-close");
+const closeMenu = document.querySelector(".burger-menu__btn-close");
 
 openMenu.addEventListener("click", () => {
+  menu.hidden = false;
   menu.classList.add("is-open");
 });
 
 closeMenu.addEventListener("click", () => {
   menu.classList.remove("is-open");
+  menu.hidden = true;
 });
 
 document.addEventListener("keydown", (event) => {
   if (event.key === "Escape") {
     menu.classList.remove("is-open");
+    menu.hidden = true;
   }
 });
